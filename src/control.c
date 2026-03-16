46dcbec5f9c54aa3f841c6ff9938421b88f716cc
azimut <azimut.github@protonmail.com>
2026-03-16 06:27:44 -0300

    refactor: MAX/MIN macro to SDL_

--- src/control.c
+++ src/control.c
@@ -1,8 +1,5 @@
 #include "control.h"
 
-#define MIN(a, b) (((a) < (b)) ? (a) : (b))
-#define MAX(a, b) (((a) > (b)) ? (a) : (b))
-
 Control control_new(const int cols, const int rows) {
   return (Control){.cols = cols, .rows = rows};
 }
@@ -13,19 +10,19 @@ void control_update(Control *control, const Control_Move movement,
   int new_ypos = control->ypos;
   switch (movement) {
   case CONTROL_MOVE_UP: {
-    new_ypos = MAX(control->ypos - 1, 0);
+    new_ypos = SDL_max(control->ypos - 1, 0);
     break;
   }
   case CONTROL_MOVE_DOWN: {
-    new_ypos = MIN(control->ypos + 1, control->rows - 1);
+    new_ypos = SDL_min(control->ypos + 1, control->rows - 1);
     break;
   }
   case CONTROL_MOVE_LEFT: {
-    new_xpos = MAX(control->xpos - 1, 0);
+    new_xpos = SDL_max(control->xpos - 1, 0);
     break;
   }
   case CONTROL_MOVE_RIGHT: {
-    new_xpos = MIN(control->xpos + 1, control->cols - 1);
+    new_xpos = SDL_min(control->xpos + 1, control->cols - 1);
     break;
   }
   }
